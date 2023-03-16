1d061ac3d2cc3a320126c33f9f03634ea130d196
J-LLK <126735084+J-LLK@users.noreply.github.com>
2023-03-16 13:28:30 +1300

    Act.2 completed

--- Act.2.py
+++ Act.2.py
@@ -1,13 +1,34 @@
+#1
 #def brrrr():
     #print("Heellloooo Vieeetttnaaaammm!!!")
 #brrrr()
+#2
 #def add_numbers():
 #    print(210+210)
 #def sub_numbers():
 #    print(1610-1200)
 #add_numbers()
 #sub_numbers()
+#3
 #def fn(first, last):
 #    print(first, last)
 #fn("Jesse", "Languasco")
-def 
+#def bam(x,y):
+#    while x > 0:
+#        print(y, end =" ")
+#        x -= 1
+#bam(9, "Why")
+#def wam(num1):
+#    total = 0
+#    for z in num1:
+#        total+=z
+#    print(total)
+#num1 = [2,9,3,1,5]
+#wam(num1)
+def food(num2):
+    total = 1
+    for z in num2:
+        total*=z
+    print(total)
+num2 = [2,9,3,1,5]
+food(num2)
